bc3a159d2118e8cdbe2e9f92a3e61c69a646e3dc
Antti <antti@antti.codes>
2024-01-20 20:50:47 +0200

    feat: split sitemap

---
+++ src/pages/train/[date]/sitemap.xml.ts
@@ -0,0 +1,51 @@
+import type { GetServerSidePropsContext } from "next";
+import { getJSON } from "~/lib/http";
+
+function generateSiteMap(paths: string[]): string {
+  return `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${paths
+      .map(
+        (path) =>
+          `
+      <url>
+          <loc>https://venaarauhassa.fi${path}</loc>
+          <changefreq>hourly</changefreq>
+      </url>`
+      )
+      .join("")}
+    </urlset>
+  `;
+}
+
+export default function SiteMap() {
+  /* empty */
+}
+
+export async function getServerSideProps({ res, query }: GetServerSidePropsContext) {
+  res.setHeader("Cache-Control", "public, s-maxage=3600, stale-while-revalidate=7200");
+
+  const date = query.date as string;
+
+  const trains = (await getJSON(`https://rata.digitraffic.fi/api/v1/trains/${date}`).catch(
+    () => []
+  )) as {
+    trainNumber: number;
+    trainType: string;
+  }[];
+
+  const trainNumbers = trains
+    .filter((t) => ["IC", "S"].includes(t.trainType))
+    .map((train) => train.trainNumber);
+
+  const paths = trainNumbers.map((n) => `/train/${date}/${n}`);
+  const sitemap = generateSiteMap(paths);
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
